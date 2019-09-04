68b27558ec6d823f2f1882ab9f8eb28f5b7e2c06
BruceTheHoon <jakobbp@gmail.com>
2019-09-04 17:15:55 +0200

    Implements CLI and emends README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MusicHomeworkGenerator
 Generator of random musical note sheets, used for generating homework when learning or teaching musical notes.
+
+## About
+Music homework generator is a *CLI* application for generating sheets of random notes, which can be used as homework practice sheets for practicing note recognition and transcription.
+Generated sheets are stored as `.png` images and enumerated by homework type and page number.
+
+## Usage
+Running the application requires *Java Runtime Environment 7* or newer.
+Assuming, that compiled classes are in the `bin` directory, it can be run like this:
+```bash
+java -cp ".;./bin" jbp.notes.NotesGenerator --type=<HW_TYPE> --pages=<N_PAGES>
+```
+
+where `<HW_TYPE>` is the type of homework and `<N_PAGES>` is the number of pages to be generated.
+
+Implemented and recognized types of homework are:
+ - **G1**: Guitar homework sheets with *TAB* bars, where all notes correspond to empty guitar strings in standard tuning.
+ - **G2**: Guitar homework sheets with *TAB* bars, where first quarter of notes correspond to empty guitar strings in standard tuning and the other notes are random.
+ - **G3**: Guitar homework sheets with *TAB* bars, where all notes are random.
--- src/jbp/notes/NotesGenerator.java
+++ src/jbp/notes/NotesGenerator.java
@@ -16,37 +16,107 @@ import jbp.notes.constants.SheetDimensions;
 import jbp.notes.model.HomeworkBars;
 import jbp.notes.model.Note;
 
-@SuppressWarnings("unused")
 public class NotesGenerator implements MusicalSymbols {
+	private static final String ARG_KEY_HELP = "--help";
+	private static final String ARG_KEY_TYPE = "--type";
+	private static final String ARG_KEY_PAGES = "--pages";
 	
 	public static void main(String[] args) throws Exception {
-//		generateType1Homework(1);
-		generateType2Homework(3);
-//		generateType3Homework(1);
+		boolean helpRequired = false;
+		String hwType = null;
+		int nPages = 0;
+		for(String arg : args) {
+			if(ARG_KEY_HELP.equals(arg)) {
+				helpRequired = true;
+				break;
+			} else if(arg.indexOf('=') != -1) {
+				String[] keyValuePair = arg.split("=");
+				if(keyValuePair.length != 2) {
+					helpRequired = true;
+					break;
+				} else if(ARG_KEY_TYPE.equals(keyValuePair[0])) {
+					hwType = keyValuePair[1];
+				} else if(ARG_KEY_PAGES.equals(keyValuePair[0])) {
+					try {
+						nPages = Integer.parseInt(keyValuePair[1]);
+					} catch(NumberFormatException e) {
+						//EMPTY
+					}
+					if(nPages < 1) {
+						System.err.println("Invalid number of pages: '"+keyValuePair[1]+"'");
+						helpRequired = true;
+						break;
+					}
+				} else {
+					System.err.println("Unrecognized argument assignment: '"+keyValuePair[0]+"'");
+					helpRequired = true;
+					break;
+				}
+			} else {
+				System.err.println("Unrecognized argument: '"+arg+"'");
+				helpRequired = true;
+				break;
+			}
+		}
+		
+		if(helpRequired) {
+			printUsageInfo();
+		} else if(hwType == null || nPages < 1) {
+			if(hwType == null) {
+				System.err.println("Missing 'type' parameter");
+			}
+			if(nPages < 1) {
+				System.err.println("Missing 'pages' parameter");
+			}
+			printUsageInfo();
+		} else {
+			generateHomework(hwType, nPages);
+		}
+	}
+	
+	private static void printUsageInfo() {
+		System.out.println("Usage: java -cp \".;./bin\" jbp.notes.NotesGenerator --type=<HW_TYPE> --pages=<N_PAGES>");
+	}
+	
+	private static void generateHomework(String hwType, int nPages) throws IOException {
+		switch(hwType) {
+		case "G1":
+			generateType1GuitarHomework(nPages);
+			break;
+		case "G2":
+			generateType2GuitarHomework(nPages);
+			break;
+		case "G3":
+			generateType3GuitarHomework(nPages);
+			break;
+		default:
+			System.err.println("Unknown 'type' value: '"+hwType+"'");
+			System.out.println("Allowed types are: G1, G2, G3");
+		}
 	}
 
-	private static void generateType1Homework(int nPages) throws IOException {
+	private static void generateType1GuitarHomework(int nPages) throws IOException {
 		int nStringNotes = 64;
 		int nAllNotes = 0;
-		String typeSuffix = "T1";
-		generateHomework(nPages, nStringNotes, nAllNotes, typeSuffix);
+		String typeSuffix = "G1";
+		generateGuitarHomework(nPages, nStringNotes, nAllNotes, typeSuffix);
 	}
 	
-	private static void generateType2Homework(int nPages) throws IOException {
+	private static void generateType2GuitarHomework(int nPages) throws IOException {
 		int nStringNotes = 16;
 		int nAllNotes = 48;
-		String typeSuffix = "T2";
-		generateHomework(nPages, nStringNotes, nAllNotes, typeSuffix);
+		String typeSuffix = "G2";
+		generateGuitarHomework(nPages, nStringNotes, nAllNotes, typeSuffix);
 	}
 	
-	private static void generateType3Homework(int nPages) throws IOException {
+	private static void generateType3GuitarHomework(int nPages) throws IOException {
 		int nStringNotes = 0;
 		int nAllNotes = 64;
-		String typeSuffix = "T3";
-		generateHomework(nPages, nStringNotes, nAllNotes, typeSuffix);
+		String typeSuffix = "G3";
+		generateGuitarHomework(nPages, nStringNotes, nAllNotes, typeSuffix);
 	}
 	
-	private static void generateHomework(int nPages, int nStringNotes, int nAllNotes, String typeSuffix) throws IOException {
+	private static void generateGuitarHomework(int nPages, int nStringNotes, int nAllNotes, String typeSuffix) throws IOException {
 		BufferedImage iBuff = new BufferedImage(SheetDimensions.SHEET_A4.width, SheetDimensions.SHEET_A4.height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D gBuff = iBuff.createGraphics();
 		gBuff.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
